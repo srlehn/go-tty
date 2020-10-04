bf5b58f5f53f1ce8141b7fe2742dd141196473f3
Simon Lehn <48837958+srlehn@users.noreply.github.com>
2020-10-04 18:09:21 +0200

    Revert "use context cancellation to cancell sigwinch on windows"
    This reverts commit 43a8d4a76caae602e3af374c22fe3551036e4d8d.

--- tty_windows.go
+++ tty_windows.go
@@ -3,7 +3,6 @@
 package tty
 
 import (
-	"context"
 	"os"
 	"errors"
 	"syscall"
@@ -124,13 +123,12 @@ type charInfo struct {
 }
 
 type TTY struct {
-	in                *os.File
-	out               *os.File
-	st                uint32
-	rs                []rune
-	ws                chan WINSIZE
-	sigwinchCtx       context.Context
-	sigwinchCtxCancel context.CancelFunc
+	in     *os.File
+	out    *os.File
+	st     uint32
+	rs     []rune
+	ws     chan WINSIZE
+	closeC chan interface{}
 }
 
 func readConsoleInput(fd uintptr, record *inputRecord) (err error) {
@@ -187,7 +185,7 @@ func open() (*TTY, error) {
 	procSetConsoleMode.Call(h, uintptr(st))
 
 	tty.ws = make(chan WINSIZE)
-	tty.sigwinchCtx, tty.sigwinchCtxCancel = context.WithCancel(context.Background())
+	tty.closeC = make(chan interface{})
 
 	return tty, nil
 }
@@ -215,17 +213,13 @@ func (tty *TTY) readRune() (rune, error) {
 			W: int(wr.size.x),
 			H: int(wr.size.y),
 		}
-
-		if err := tty.sigwinchCtx.Err(); err != nil {
-			// closing
-			// the following select might panic without this guard close
-			return 0, err
+		select {
+		case <-tty.closeC: // closing
+			return 0, nil
+		default:
 		}
-
 		select {
 		case tty.ws <- ws:
-		case <-tty.sigwinchCtx.Done():
-			return 0, tty.sigwinchCtx.Err()
 		default:
 			return 0, nil // no one is currently trying to read
 		}
@@ -327,7 +321,7 @@ func (tty *TTY) readRune() (rune, error) {
 
 func (tty *TTY) close() error {
 	procSetConsoleMode.Call(tty.in.Fd(), uintptr(tty.st))
-	tty.sigwinchCtxCancel()
+	close(tty.closeC)
 	close(tty.ws)
 	return nil
 }
